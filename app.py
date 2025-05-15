fbd3f21bcda6ea53f288cf66987eb75dd50dec41
Alex Bunting <alexander_bunting@hotmail.com>
2025-05-15 10:18:02 +0200

    Added delete movie route

--- app.py
+++ app.py
@@ -74,5 +74,51 @@ def add_movie(user_id):
     return render_template('add_movie.html', user=user)
 
 
+@app.route('/users/<int:user_id>/edit_movie/<int:movie_id>', methods=['GET', 'POST'])
+def edit_movie(user_id, movie_id):
+    user = data_manager.get_user(user_id)
+    movie = data_manager.get_movie(movie_id)
+
+    if not user or not movie or movie.user_id != user.id:
+        abort(404)
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        director = request.form.get('director') or ''
+        year = request.form.get('year') or ''
+        rating = request.form.get('rating') or ''
+
+        if title:
+            try:
+                api_key = os.getenv('OMDB_API_KEY')
+                url = f'http://www.omdbapi.com/?apikey={api_key}&t={title}'
+                response = requests.get(url)
+                data = response.json()
+                director = data.get('Director') or director
+                year = data.get('Year') or year
+                rating = data.get('imdbRating') or rating
+                print(f"OMDb raw response: {data}")
+            except:
+                pass
+
+            data_manager.update_movie(movie_id, title, director, year, rating)
+            return redirect(url_for('user_movies', user_id=user.id))
+
+    return render_template('edit_movie.html', user=user, movie=movie)
+
+
+
+@app.route('/users/<int:user_id>/delete_movie/<int:movie_id>')
+def delete_movie(user_id, movie_id):
+    user = data_manager.get_user(user_id)
+    movie = data_manager.get_movie(movie_id)
+
+    if not user or not movie or movie.user_id != user.id:
+        abort(404)
+
+    data_manager.delete_movie(movie_id)
+    return redirect(url_for('user_movies', user_id=user.id))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
